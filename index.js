faf6792b6d5550048794aa58232f431da58125b2
elango09092004 <elango.t2022cce@sece.ac.in>
2024-08-13 11:22:06 +0530

    added mongodb atlas

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const eventroutes=require("./routes/eventroutes")
 const bookingroutes=require("./routes/bookingroute")
 
 //mongodb connection
-mongoose.connect("mongodb://localhost:27017/bookMyEvent")
+mongoose.connect("mongodb+srv://elango:e-commerce@cluster0.mll0g.mongodb.net/e-commerce")
     .then(() => console.log("Connected to MongoDB..."))
     .catch(err => console.error('Could not connect to MongoDB... ', err));
 
